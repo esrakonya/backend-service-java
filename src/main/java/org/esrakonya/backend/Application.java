bf884b741121efcb5dce61583b1ce2b2f8998949
esrakonya <esrakonya6@gmail.com>
2026-01-21 09:55:34 +0300

    chore: migrate project to Spring Boot 3.2.5 and add web starter

--- src/main/java/org/esrakonya/backend/Application.java
+++ src/main/java/org/esrakonya/backend/Application.java
@@ -2,18 +2,24 @@ package org.esrakonya.backend;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        String environment = System.getenv("APP_ENV");
+        String env = System.getenv("APP_ENV");
 
-        if (environment == null) {
-            environment = "local";
+        if (env == null) {
+            env = "LOCAL";
         }
 
-        logger.info("Application started in environment: {}", environment);
+        //Testing different logging levels
+        logger.debug("DEBUG LEVEL: This message is for developers only.");
+        logger.info("INFO LEVEL: Application started successfully in {} environment.", env);
+        logger.warn("WARN LEVEL: This is a warning message. Potential issue detected.");
+        logger.error("ERROR LEVEL: Critical error occurred! Action required.");
     }
 }
