e7d82c08d7e4b8de6543d040eb7cc08dbe94d7d6
esrakonya <esrakonya6@gmail.com>
2026-06-13 09:27:50 +0300

    build: finalize centralized Testcontainers config for M3 Mac and Docker v29

--- marketplace-common-core/src/main/java/org/esrakonya/backend/common/core/exception/InsufficientStockException.java
+++ marketplace-common-core/src/main/java/org/esrakonya/backend/common/core/exception/InsufficientStockException.java
@@ -1,7 +1,5 @@
 package org.esrakonya.backend.common.core.exception;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
-
 public class InsufficientStockException extends BaseException {
 
     public InsufficientStockException(String message) {
